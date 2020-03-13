e41452aefe643b1662c101104bd102ebd1c75dfd
深瀬 貴将 <fukase@arsaga.jp>
2020-03-13 17:07:08 +0900

    Initial Commit

---
+++ InabaFlippingCardGame-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InabaFlippingCardGame-SwiftUI
+//
+//  Created by 深瀬 貴将 on 2020/03/13.
+//  Copyright © 2020 深瀬 貴将. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
