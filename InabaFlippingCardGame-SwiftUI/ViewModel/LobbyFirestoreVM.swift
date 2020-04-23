de88d11407ac39432235a0092f997f311c06c24b
深瀬 貴将 <fukase@arsaga.jp>
2020-04-24 01:46:25 +0900

    ロビー画面のVMを作成。Firestore経由でDB上のルーム一覧をリアルタイム取得する　またルームの配列を保持

--- InabaFlippingCardGame-SwiftUI/ViewModel/LobbyFirestoreVM.swift
+++ InabaFlippingCardGame-SwiftUI/ViewModel/LobbyFirestoreVM.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import Firebase
+
+
+class LobbyFirestoreVM: ObservableObject {
+    @Published var rooms = [Rooms]()
+    
+    let db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+        //ロビーに表示するオンラインルーム一覧情報の自動更新を設定
+        db.collection("rooms").order(by: "defaultRoom").addSnapshotListener{ snapshot, err in
+            guard let snapshot = snapshot else {
+                print("snapshotListener Error: \(String(describing: err))"); return
+            }
+            self.rooms = snapshot.documents.map { data -> Rooms in
+                guard let roomName = data.data()["roomName"] else {
+                    print("roomNameアンラップ失敗")
+                    return Rooms(documentID: "nil", roomName: "nil", playerCount: 0)
+                }
+                return Rooms(documentID: data.documentID, roomName: roomName as! String, playerCount: data.data().count - 3)
+            }
+        }
+        
+    }
+}
+
+extension LobbyFirestoreVM {
+    
+}
